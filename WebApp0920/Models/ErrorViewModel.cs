ef98268d88264746bf0b479c1fe21bd98f7653d7
Harpreet Singh <harpreet_kang83@outlook.com>
2020-09-29 21:29:22 -0500

    add project

---
+++ WebApp0920/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp0920.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
